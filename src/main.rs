e98ae1e0fc0cf9463f698850961ec1b07561dcb1
Дамјан Георгиевски <gdamjan@gmail.com>
2018-06-28 22:48:53 +0200

    bind_addr optionaly via HTTP_ADDR env var

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,22 @@ extern crate actix_web;
 extern crate uuid;
 extern crate futures;
 
+use std::env;
+
 mod webapp;
 mod rpssl;
 
 fn main() {
     let sys = actix::System::new("rpssl");
-    let addr = "127.0.0.1:8088";
+    let bind_addr = env::var("HTTP_ADDR").unwrap_or("127.0.0.1:8088".to_string());
 
     let actor_addr = rpssl::start();
 
     actix_web::server::new(move || webapp::create_app(actor_addr.clone()))
-        .bind(addr)
+        .bind(&bind_addr)
         .unwrap()
         .start();
 
-    println!("Started http server: http://{}/", addr);
+    println!("Started http server: http://{}/", &bind_addr);
     let _ = sys.run();
 }
